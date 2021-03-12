3e51fac2d980e6f330c4a8b80f5ede1f97758d1f
Vekyork <vekyork@yandex.ru>
2021-03-13 02:38:34 +0300

    Lesson3

---
+++ src/main/java/ru/geekbrains/lesson4/Main.java
@@ -0,0 +1,36 @@
+package ru.geekbrains.lesson4;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 0;
+        int i;
+
+        Person[] persArray = new Person[5];
+        persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+        persArray[1] = new Person("Petrov Petr", "Military", "petrov.p@mailbox.com", "891600000", 28000, 45);
+        persArray[2] = new Person("Nikolaev Nikolay", "Driver", "nikolaev.n@mailbox.com", "890100000", 32000, 28);
+        persArray[3] = new Person("Vetrov Vasily", "Worker", "vetrov.v@mailbox.com", "895100000", 20000, 18);
+        persArray[4] = new Person("Sidorov Stepan", "Scientist", "sidorov.s@mailbox.com", "890300000", 100000, 54);
+
+
+//        while (age > 40) {
+
+//        }            System.out.println(persArray);
+
+        for (i = 0; i < persArray.length; i++) {
+            System.out.println(persArray[i]);
+            if (age > 40) {
+                    System.out.println(persArray[i]);
+                }
+
+        }
+    }
+}
+
+
+
+
+
---
+++ src/main/java/ru/geekbrains/lesson4/Person.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.lesson4;
+
+public class Person {
+    private String fio;
+    private String doljnost;
+    private String email;
+    private String telefon;
+    private int zarplata;
+    private int age;
+
+
+    public Person(String fio, String doljnost, String email, String telefon, int zarplata, int age) {
+        this.fio = fio;
+        this.doljnost = doljnost;
+        this.email = email;
+        this.telefon = telefon;
+        this.zarplata = zarplata;
+        this.age = age;
+    }
+}
+
